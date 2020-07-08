0275e9c2633a9eee3a6f877ca601f40f232e519f
mauriciodesign <mauricio.a.design@gmail.com>
2020-07-08 13:15:50 -0400

    Se agrega bootstrap, cambio en diseño, cambio en nombre de arreglo y funciones

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="main">
-    <h2>{{ message }}</h2>
-    <br />
-    <label>Tarea</label>
-    <input type="text" placeholder="Ingrese nueva tarea" v-model="tarea" />
-    <button @click="guardarTarea">Crear</button>
-    <br />
-
-    <h3>Lista</h3>
-    <ul class="container">
-        <li v-for="(tarea, index) in tareas" :key="index">
-            <input type="checkbox">{{index}}:{{tarea.name}}
-        </li>
-    </ul>
+  <div class="todolist">
+    <div class="todolist__main">
+      <div class="container">
+        <h2>{{ message }}</h2>
+        <br />
+        <form class="todolist__search">
+            <input type="text" id="input" placeholder="Ingrese una nueva tarea" v-model="task" @keyup.enter.prevent="saveToDo"/>
+            <button class="btn btn-primary" @click.prevent="saveToDo">Crear</button>
+        </form>
+        <ul class="container">
+            <li v-for="(tarea, index) in toDoList" :key="index">
+                <input type="checkbox">{{index}}:{{tarea.name}}
+            </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -20,51 +22,89 @@ export default {
   data() {
     return {
       message: "Crear una nueva tarea",
-      tarea: "",
-      tareas: [],
-      mostrar: false
+      task: "",
+      toDoList: [],
     };
   },
   methods: {
     mostrarNombre: function() {
       this.mostrar = !this.mostrar;
     },
-    guardarTarea: function() {
-        if(this.tarea !== ""){
-
-            this.tareas.push({
-              name: this.tarea
-            });
-        }
-      this.tarea = "";
-    }
+    saveToDo() {
+      if (this.task !== "") {
+        this.toDoList.push({ name: this.task, done: false });
+        this.show = true
+      }
+      this.task = "";
+    },
   }
 };
 </script>
 <style scoped>
-#main {
-  font-size: 25px;
-  font-family: "Calibri", sans-serif;
-  color: darkblue;
+.todolist {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  height: 100vh;
+}
+.todolist__main {
+  color: #415c79;
   text-align: center;
+  background: #fff;
+  border-radius: 20px;
+  padding: 50px;
+  animation-name: opacity;
+  animation-duration: 1.5s;
+  box-shadow: 0 2px 43px -4px rgba(0, 0, 0, 0.19) !important;
 }
-input{
-    padding: 10px;
-    margin: 0 10px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
+.todolist__search {
+    -webkit-box-align: center;
+    align-items: center;
+    background: #f1f1f1;
+    border-radius: 10px;
+    border: 2px solid #ffffff;
+    display: -webkit-box;
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    margin: 0.5em 0;
+    padding: 0.5em 0.5em 0.5em 1em;
+    transition: all 0.5s;
+    -webkit-transition: all 0.5s;
+    width: 450px
 }
-button{
-    padding: 10px 30px;
-    background: #0e62ff;
-    border-radius: 5px;
-    border: none;
+.todolist__search:hover,
+.todolist__search:focus {
+    border: 2px solid #e6e6e6;
+}
+
+.todolist__search button,
+.todolist__search input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;    
+    border: 0;
+    color: inherit;
+    font-size: 1.5rem;
+    outline: 0
+}
+.todolist__search button {
+    cursor: pointer;
+    padding: 0.2em 0.8em;
+    border-radius: 10px;
     color: #fff;
+    font-size: 1.3rem;
 }
-.container{
-    width: 30%;
-    margin: 30px auto 0 auto;
-    text-align: left;
-    padding-left: 132px;
+.todolist__search input {
+    -webkit-box-flex: 1;
+    flex: 1;
+    background: transparent;
+    font-weight: 500;
+    font-size: 23px
+}
+
+.todolist__search input::placeholder {
+    color: #c2c2c2
 }
 </style>
